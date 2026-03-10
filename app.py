6a732b25cd4a246c2ccbc315a766ac44ad84a425
STUDY GROUP <premjeetrajbhar2@gmail.com>
2026-03-10 12:33:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 import os
 from flask import Flask
-from threading import Thread
 
 app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def home():
-    return "Bot Running"
+    return "Hello, Render!"
 
-def run():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-# Flask server start
-Thread(target=run).start()
-
-# ---- Telegram Bot Code ----
-from pyrogram import Client
-
-bot = Client("mybot")
-
-bot.start()
-print("Bot Started")
-
-bot.idle()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
